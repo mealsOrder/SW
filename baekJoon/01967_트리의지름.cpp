764a6139cb9dc4f127d1c019fa601ca26d43e182
MealsOrder <dbswo7007@gmail.com>
2024-10-11 16:57:21 +0900

    2024-10-11 : Prepare for KT Coding Test01

--- baekJoon/01967_트리의지름.cpp
+++ baekJoon/01967_트리의지름.cpp
@@ -1,63 +1,56 @@
 #include<iostream>
-#include<algorithm>
 #include<vector>
-#include<queue>
+
 using namespace std;
 const int MX = 10'001;
-const int INF = 987987987;
-int N;
 vector<pair<int,int>>v[MX];
-int dist[MX];
-int ans;
+bool vis[MX];
+int N,mxNode,mxDist;
+void DFS(int node,int dist){
+    // 이미 방문했으면 종료
+    if(vis[node])return;
+    vis[node] = true; // 방문 처리
 
-
-int BFS(int x,int y){
-    fill(dist,dist+N+1,INF);
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>> > pq;
-    dist[x] = 0;
-    pq.push({dist[x],x});
-
-    while(!pq.empty()){
-        int curCost = pq.top().first;
-        int curNode = pq.top().second;
-        pq.pop();
-        if(curNode == y) return dist[y];
-
-        for(int i=0;i<v[curNode].size();i++){
-            int nxCost = v[curNode][i].second;
-            int nxNode = v[curNode][i].first;
-
-            if(dist[nxNode] > dist[curNode]+nxCost){
-                dist[nxNode] = dist[curNode]+nxCost;
-                pq.push({dist[nxNode],nxNode});
-            }
-        }
+    // 현재 거리가 최대거리보다 크면 갱신
+    if(dist > mxDist ){
+        mxDist = dist;
+        mxNode = node;
     }
 
-    return INF;
+    // 다음 노드, 다음 거리
+    for(int i=0;i<v[node].size();i++){
+        int nxNode = v[node][i].first;
+        int nxDist = v[node][i].second;
+
+        // 다음 노드, 누적거리로 DFS
+        DFS(nxNode,dist+nxDist);
+    }
 }
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
+
     cin >> N;
-    int T = N -1;
+    int T = N-1;
+
     while(T--){
         int a,b,c;
-        cin >> a >> b>> c;
+        cin >> a>>b>>c;
 
         v[a].push_back({b,c});
         v[b].push_back({a,c});
     }
 
-    for(int i=1;i<=N;i++){
-        for(int j=1;j<=N;j++){
-            if(i == j)continue;
-            int k = BFS(i,j);
-            if(k != INF) ans = max(ans,k);
-        }
-    }
+    DFS(1,0); // (시작 노드, 거리)
+    
+    // init
+    fill(vis,vis+N+1,0);
+    
+    // 1부터 가장 멀리 있는 노드부터 최대거리
+    DFS(mxNode,0);
 
-    cout << ans << '\n';
+    cout << mxDist << '\n';
 
     return 0;
 }
